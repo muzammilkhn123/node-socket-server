5494e4a03cfcfe898e726ddd9cae728a6473305d
muzammilkhan1 <email@email.com>
2021-04-23 16:38:11 +0500

    socket updated

--- index.js
+++ index.js
@@ -13,34 +13,76 @@ let ON_DISCONNECTION= "disconnect";
 
 // Main Events
 let EVENT_IS_USER_ONLINE= "check_online";
-let EVENT_SINGLE_CHAT_MESSAGE= "signle chat message";
+let EVENT_SINGLE_CHAT_MESSAGE= "single_chat_message";
 
 // Sub Events
-let SUB_EVENT_RECEIVE_MESSAGE= "message";
+let SUB_EVENT_RECEIVE_MESSAGE= "receive_message";
 let SUB_EVENT_IS_USER_CONNECTED= "is_user_connected";
 
   const usersMap= new Map();
  
+//----------------------------------------HELPER FUNCTION FOR SOCKET-----------------------------------------------
+function addUserToUsersMap(key_user_id,valueForUserMap){
+   usersMap.set(key_user_id, valueForUserMap);
+   console.log(usersMap);
+}
+
+
+function singleChatHandler(client,chat_message){
+  printClass.printOnMessage(chat_message); 
+  let receiverSocketID= getSocketIDFomMap(chat_message.ReceiverID);
+  if(receiverSocketID== undefined){
+    printClass.printUserNotConnected();
+     chat_message.to_user_online_status=false;
+     return;
+  }
+  chat_message.to_user_online_status=true;
+  sendMessageToReceiver(client,receiverSocketID,SUB_EVENT_RECEIVE_MESSAGE,chat_message); 
+} 
+
+
+function sendMessageToReceiver(client,receiverSocketID,event,chat_message){
+  client.to(receiverSocketID).emit(event,JSON.stringify(chat_message) );
+} 
+
+
+function getSocketIDFomMap(userID){
+let socketIDOFUser=usersMap.get(`${userID}`)
+if(socketIDOFUser == undefined){
+  return undefined;
+    }
+return socketIDOFUser.socket_client_id;
+}
+
+
 //----------------------------------------SOCKET SERVER-----------------------------------------------
 io.on(ON_CONNECTION, function (client) {
-
-  // console.log('client connect...', client.id);
   printClass.onConnectionPrint(client);
+  var from_user_id= client.handshake.query.from;
+  let valueForUserMap= {socket_client_id:client.id};
+  addUserToUsersMap(from_user_id,valueForUserMap);
+  printClass.printOnlineUsersNumber(usersMap);
+
   
+  client.on(EVENT_SINGLE_CHAT_MESSAGE, function (chat_message){
+     singleChatHandler(client,chat_message)
+     
+    })
+
   client.on('typing', function name(data) {
     console.log(data);
     io.emit('typing', data)
   })
 
   client.on(SUB_EVENT_RECEIVE_MESSAGE, function name(data) {
-    console.log(data);
-    io.emit(SUB_EVENT_RECEIVE_MESSAGE, data)  
+     singleChatHandler(socket,chat_message); 
+     io.emit(SUB_EVENT_RECEIVE_MESSAGE, data)  
   })
 
   client.on('location', function name(data) {
     console.log(data);
     io.emit('location', data);
-  })
+  }) 
 
   client.on('connect', function () {
   })
--- printClass.js
+++ printClass.js
@@ -2,12 +2,30 @@ module.exports = class PrintClass
 {
 
     onConnectionPrint(client){
-        console.log('----------------------------------------------------------------------------------------');
+        console.log('\n-------------------------------------------------------------------------------------------------');
         console.log("Connected => Socket ID: "+ client.id + ", User: "+ JSON.stringify(client.handshake.query));
     }
 
     onDisconnectionPrint(client){
-        console.log('----------------------------------------------------------------------------------------');
+        console.log('\n-------------------------------------------------------------------------------------------------');
         console.log("Disconneced => Socket ID: " + client.id);
+    }
+
+    printOnlineUsersNumber(usersMap){
+        console.log('-------------------------------------------------------------------------------------------------');
+        console.log("Users Online: " + usersMap.size);
+
+    }
+    printOnMessage(chat_message){
+        console.log('-------------------------------------------------------------------------------------------------');
+        console.log("On Message: " + JSON.stringify(chat_message));
+        let receiverID= chat_message.ReceiverID;
+     let senderID= chat_message.senderID;
+     console.log("Sender: "+senderID +"|| Receiver: " +receiverID  );
+
+    }
+    printUserNotConnected(){
+        console.log('-------------------------------------------------------------------------------------------------');
+        console.log("Chat User Not Connected");
     }
 }   
